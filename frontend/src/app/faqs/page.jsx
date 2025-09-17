ac270604456b1f70502d4e4db367f63a0b349be1
Your Name <nazrulislam.cse28@gmail.com>
2025-09-18 02:42:37 +0600

    Project updated

--- frontend/src/app/faqs/page.jsx
+++ frontend/src/app/faqs/page.jsx
@@ -1,4 +1,5 @@
 
+import Faqs from '@/components/faqs/Faqs'
 import Faq from '@/components/LandingPage/Faq'
 import CatagoriesSlider from '@/components/UserLandingPage/UserLandingPageCard/CatagoriesSlider'
 import React from 'react'
@@ -6,6 +7,7 @@ import React from 'react'
 const FAQSPage = () => {
   return (
     <div>
+      <Faqs/>
       <Faq/>
       <CatagoriesSlider/>
     </div>

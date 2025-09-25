a94ddd3e5bbefa35550c194644c70ea3c5e9c00d
Your Name <nazrulislam.cse28@gmail.com>
2025-09-25 06:02:22 +0600

    Project updated

--- frontend/src/components/dialog/DialogDemo.jsx
+++ frontend/src/components/dialog/DialogDemo.jsx
@@ -1,55 +1,50 @@
-"use client";
-
-import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogClose,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { FaGift } from "react-icons/fa6";
 import { LuSend } from "react-icons/lu";
-import Confetti from "react-confetti";
+import { ConfettiButton } from "../ui/confetti";
+import Image from "next/image";
 
 export function DialogDemo() {
-  const [open, setOpen] = useState(false);
-
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog>
       <form>
         <DialogTrigger asChild>
-          <Button className="bg-[#00308F] text-white font-bold text-lg lg:w-[600px] py-4 cursor-pointer">
+          <Button className="bg-[#00308F] text-white font-bold text-lg lg:w-[600px] py-6 cursor-pointer">
             View Coupon
             <LuSend className="mt-1" />
           </Button>
         </DialogTrigger>
-
-        <DialogContent className="sm:max-w-[425px] relative overflow-hidden">
-          {/* 🎉 Confetti animation only when modal is open */}
-          {open && (
-            <div className="absolute top-0 left-0 w-full h-32 pointer-events-none">
-              <Confetti
-                width={400} // Adjust width
-                height={150} // Confetti height only at top
-                numberOfPieces={80}
-                recycle={false} // Runs only once when opened
-                gravity={0.3}
-              />
-            </div>
-          )}
-
+        <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
-            <div className="flex justify-center text-3xl text-[#00308F] mt-10 mb-3">
+            <div className="flex justify-center text-3xl text-[#00308F] mt-10 mb-3 relative">
               <FaGift />
+              <div className="absolute bottom-[-160px] w-[430px]">
+                <Image
+                src={'/cong.png'}
+                width={500}
+                height={400}
+                alt="cong_image"
+                className="w-full"
+                objectFit="cover"
+                />
+              </div>
             </div>
-            <DialogTitle className="text-center text-[#7D7878]">
+            <DialogTitle className="text-center text-gray-700 font-bold">
               Surprise gift for you
             </DialogTitle>
-            <h1 className="text-center text-4xl font-bold text-[#00308F] mt-8">
+            <h1 className="text-center text-4xl font-bold text-[#00308F] mt-6">
               50% OFF
             </h1>
             <h3 className="text-xl mb-10 text-center">Entire Purchase</h3>
@@ -65,12 +60,14 @@ export function DialogDemo() {
 
           <DialogFooter className="mt-16 mb-4">
             <DialogClose asChild>
-              <Button
-                className="bg-[#00308F] py-6 text-white text-xl w-full cursor-pointer"
-                variant="outline"
-              >
-                Redeem Coupon
-              </Button>
+              <div className="relative w-full">
+                <ConfettiButton
+                  className="bg-[#00308F] py-6 text-white text-xl w-full cursor-pointer"
+                  variant="outline"
+                >
+                  Redeem Coupon 🎁
+                </ConfettiButton>
+              </div>
             </DialogClose>
           </DialogFooter>
         </DialogContent>

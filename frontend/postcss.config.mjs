37dc858d3e1fa013c81c673120901b3fa604c6f3
Your Name <nazrulislam.cse28@gmail.com>
2025-11-21 04:24:45 +0600

    Project updated

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,8 +1,14 @@
-const config = {
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
+
+// export default config;
+
+
+module.exports = {
   plugins: {
-      "@tailwindcss/postcss": {},
+    "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
 };
 
-export default config;

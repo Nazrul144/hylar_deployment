761bc8c0ca8d6feb3d4999f70d37b7026c398ec9
Your Name <nazrulislam.cse28@gmail.com>
2025-12-10 04:50:21 +0600

    Dark theme issue solved

--- frontend/src/components/shared/NavHeader.jsx
+++ frontend/src/components/shared/NavHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const NavHeader = () => {
   return (
-    <div className='w-full h-10 bg-[#F5F6F8] flex items-center justify-center'>
+    <div className='w-full h-10 bg-[#F5F6F8] dark:text-gray-300 dark:bg-gray-800 flex items-center justify-center'>
       <h1 className='text-sm '>WELCOME TO MAXIMUMSAVINGS!</h1>
     </div>
   )
